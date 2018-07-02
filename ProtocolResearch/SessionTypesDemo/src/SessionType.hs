2528e725b733c23b09b452fc0d8b7e191bac95cf
olligobber <sirolligobbervii@yahoo.com.au>
2018-07-02 14:49:05 +1000

    Added subtyping, dual, and some tests to Session Types Demo

--- ProtocolResearch/SessionTypesDemo/src/SessionType.hs
+++ ProtocolResearch/SessionTypesDemo/src/SessionType.hs
@@ -1,22 +1,24 @@
 module SessionType
     ( Type(..)
-    , SessionType(..)
+    , Branches
+    , SessionType
+    , end, send, recv, offer, choose, mu, ref, join
     , isValid
-    -- , (<:)
-    -- , isSubType
-    -- , dual
-    -- , (<=>)
-    -- , isCompatible
+    , (<:)
+    , isSubType
+    , dual
+    , (<=>)
+    , isCompatible
     -- , union
     -- , smartUnion
     -- , strictUnion
-    -- , recurse -- Infinite types are messy and impractical
+    -- , simpleUnion
     ) where
 
 import qualified Data.Map.Merge.Strict as Merge
 import qualified Data.Map.Strict as Map
 import Data.Map.Strict (Map)
-import Data.Fix (Fix, cata)
+import Data.Fix (Fix(..), unFix, cata)
 import Control.Arrow ((>>>))
 import Control.Applicative (liftA2)
 
@@ -37,7 +39,7 @@ data SessionTypeF a
     | Recv Type -- ? Type
     | Choose (BranchesF [a]) -- +{String:SessionType}
     | Offer (BranchesF [a]) -- &{String:SessionType}
-    | FixP [a] -- Fixpoint
+    | Mu [a] -- Fixpoint
     | Ref Integer -- Reference to Fixpoint
         deriving (Show, Eq, Read)
 
@@ -46,11 +48,35 @@ instance Functor SessionTypeF where
     fmap _ (Recv t)     = Recv t
     fmap f (Choose m)   = Choose (fmap (fmap f) m)
     fmap f (Offer m)    = Offer (fmap (fmap f) m)
-    fmap f (FixP s)     = FixP (fmap f s)
+    fmap f (Mu s)       = Mu (fmap f s)
     fmap _ (Ref n)      = Ref n
 
 type SimpleSession = Fix SessionTypeF
 type SessionType = [SimpleSession]
+
+end :: SessionType
+end = []
+
+send :: Type -> SessionType
+send = pure . Fix . Send
+
+recv :: Type -> SessionType
+recv = pure . Fix . Recv
+
+choose :: Branches -> SessionType
+choose = pure . Fix . Choose
+
+offer :: Branches -> SessionType
+offer = pure . Fix . Offer
+
+mu :: SessionType -> SessionType
+mu = pure . Fix . Mu
+
+ref :: Integer -> SessionType
+ref = pure . Fix . Ref
+
+join :: SessionType -> SessionType -> SessionType
+join = (++)
 
 -- Determine if branches are valid and return largest reference
 isValidBranches :: BranchesF [Maybe Integer] -> Maybe Integer
@@ -60,7 +86,7 @@ isValidBranches m =
         &&  Map.notMember "" m -- No empty keys
     then
         -- Check contents are valid and return largest reference
-        foldl1 (liftA2 max) (foldl1 (liftA2 max) <$> m)
+        foldl (liftA2 max) (Just 0) (foldl (liftA2 max) (Just 0) <$> m)
     else
         Nothing
 
@@ -69,7 +95,7 @@ isValidSimple :: SimpleSession -> Maybe Integer
 isValidSimple = cata $ \x -> case x of
     Choose m    -> isValidBranches m
     Offer m     -> isValidBranches m
-    FixP ss     -> pred <$> foldl1 (liftA2 max) ss
+    Mu ss       -> pred <$> foldl (liftA2 max) (Just 0) ss
     Ref n       -> if n > 0
                     then Just n
                     else Nothing
@@ -79,51 +105,50 @@ isValidSimple = cata $ \x -> case x of
 isValid :: SessionType -> Bool
 isValid =
         fmap isValidSimple -- Get validity and largest reference of each
-    >>> foldl1 (liftA2 max) -- Get largest reference of all
+    >>> foldl (liftA2 max) (Just 0) -- Get largest reference of all
     >>> fmap (>0) -- Check if any references are out of bounds
     >>> (== Just False) -- Valid if largest wasn't out of bounds
 
 -- Left is subtype of Right
--- infix 9 <:
--- (<:) :: SessionType -> SessionType -> Bool
--- (<:) = isSubType
+infix 8 <:
+(<:) :: SessionType -> SessionType -> Bool
+(<:) = isSubType
 
 -- Determines if the first is a subtype of the second
--- isSubType :: SessionType -> SessionType -> Bool
--- isSubType x y = case (unFix x, unFix y) of
--- -- The subtype chooses from fewer options
---     (Choose m, Choose n)    -> Map.isSubMapOfBy isSubType m n
--- -- The subtype offers extra options
---     (Offer m, Offer n)      -> Map.isSubMapOfBy (flip isSubType) n m
---     (Send t1 a, Send t2 a)  -> t1 == t2 && isSubType a b
---     (Recv t1 a, Recv t2 a)  -> t1 == t2 && isSubType a b
---     (Fix a, Fix b)          ->
---     (a,b)                   -> a == b
+isSubTypeSimple :: (SimpleSession, SimpleSession) -> Bool
+isSubTypeSimple (x,y) = case (unFix x, unFix y) of
+    -- The subtype chooses from fewer options
+    (Choose m, Choose n)    -> Map.isSubmapOfBy isSubType m n
+    -- The subtype offers extra options
+    (Offer m, Offer n)      -> Map.isSubmapOfBy (flip isSubType) n m
+    (Mu a, Mu b)            -> isSubType a b
+    (Ref k, Ref l)          -> k == l
+    (a,b)                   -> a == b
 
--- -- The subtype chooses from fewer options
--- isSubType (Choose m) (Choose n)     = Map.isSubmapOfBy isSubType m n
--- -- The subtype offers extra options
--- isSubType (Offer m) (Offer n)       = Map.isSubmapOfBy (flip isSubType) n m
--- isSubType (Send t1 a) (Send t2 b)   = t1 == t2 && isSubType a b
--- isSubType (Recv t1 a) (Recv t2 b)   = t1 == t2 && isSubType a b
--- isSubType a b                       = a == b
---
--- -- Constructs the dual of a SessionType
--- dual :: SessionType -> SessionType
--- dual Wait = Kill
--- dual Kill = Wait
--- dual (Send t a) = Recv t (dual a)
--- dual (Recv t a) = Send t (dual a)
--- dual (Choose m) = Offer (dual <$> m)
--- dual (Offer m) = Choose (dual <$> m)
---
--- infix 9 <=>
--- (<=>) :: SessionType -> SessionType -> Bool
--- (<=>) = isCompatible
---
--- -- Determines if two types can communicate
--- isCompatible :: SessionType -> SessionType -> Bool
--- isCompatible a b = a <: dual b -- && b <: dual a (These are equivalent)
+-- Determines if the first is a subtype of the second
+isSubType :: SessionType -> SessionType -> Bool
+isSubType x y = (length x == length y) && (all isSubTypeSimple $ zip x y)
+
+-- Constructs the dual of a simple session type
+dualSimple :: SimpleSession -> SimpleSession
+dualSimple = cata $ \x -> case x of
+    Send t      -> Fix $ Recv t
+    Recv t      -> Fix $ Send t
+    Choose m    -> Fix $ Offer m
+    Offer m     -> Fix $ Choose m
+    s           -> Fix s
+
+-- Constructs the dual of a SessionType
+dual :: SessionType -> SessionType
+dual = fmap dualSimple
+
+infix 9 <=>
+(<=>) :: SessionType -> SessionType -> Bool
+(<=>) = isCompatible
+
+-- Determines if two types can communicate
+isCompatible :: SessionType -> SessionType -> Bool
+isCompatible a b = a <: dual b -- && b <: dual a (These are equivalent)
 --
 -- strictUnion' :: Branches -> Branches -> Maybe Branches
 -- strictUnion' m n
@@ -173,8 +198,3 @@ isValid =
 -- simpleUnion :: [(String, SessionType)] -> Maybe SessionType
 -- simpleUnion [] = Nothing
 -- simpleUnion list = Just $ Offer (Map.fromList list)
---
--- {-  Turns a looping or recursive session type into a fully defined
---     (possibly infinite) session type -}
--- recurse :: (SessionType -> SessionType) -> SessionType
--- recurse = fix
